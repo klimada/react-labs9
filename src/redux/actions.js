987afc660e067f553de7de3a7ac9a6afebeac950
klimada <56273947+klimada@users.noreply.github.com>
2019-12-15 11:28:34 +0100

    Lab10task1

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,10 +1,20 @@
 import { EMPLOYEES_LOADED } from './constants';
+import { EMPLOYEE_ADDED } from './constants';
 
 export const employeesLoaded = (employees) => {
   return {
     type: EMPLOYEES_LOADED,
     payload: {
       employees
+    }
+  };
+}
+
+export const newEmployeeAdded = (newEmployee) => {
+  return {
+    type: EMPLOYEE_ADDED,
+    payload: {
+      newEmployee
     }
   };
 }
